671f1a4e0a5f4476c9f672ec51d2d95c8f61b481
Panagiotis Fytas <p.fytas@datahow.ch>
2025-02-18 12:21:00 +0000

    add biotriplex

--- src/tasks/biotriplex/nerrel_dataset.py
+++ src/tasks/biotriplex/nerrel_dataset.py
@@ -1,10 +1,6 @@
 import copy
 import json
 import os
-import torch
-
-# from sentencepiece import SentencePieceProcessor
-from torch.utils.data import Dataset
 
 
 class biotriplex_nerrel_dataset:
@@ -59,8 +55,8 @@ INSTRUCTION = """**Extract Named Entities and Relations**: Identify and extract 
 """
 
 SYS_PROMPT = """You are a helpful assistant that extracts the list of entities in the form of {"span":<entity_text>, "entity_type":<"GENE"|"DISEASE"|"RELATION">} json entries. If no triplets are found, please provide an empty list. """
-class BioTriplexNERRELDataset(Dataset):
-    def __init__(self, dataset_config, tokenizer, split_name, max_words=None):
+class BioTriplexNERRELDataset():
+    def __init__(self, dataset_config, split_name, max_words=None):
         #self.data = json.load(open(dataset_config.data_path))
 
         if split_name == "train":
@@ -105,24 +101,12 @@ class BioTriplexNERRELDataset(Dataset):
         self.num_negative_examples = len([item for item in self.data if item["output"] == "[]"])
         self.num_positive_examples = len(self.data) - self.num_negative_examples
         self.max_words = max_words
-        self.tokenizer = tokenizer
         # self.num_truncated_examples = 0
         # self.longest_input = 0
         # self.input_seen = set()
         self.upweight_minority_class = dataset_config.upweight_minority_class
         self.use_entity_tokens_as_targets = dataset_config.use_entity_tokens_as_targets
         self.entity_special_tokens = dataset_config.entity_special_tokens
-        if dataset_config.use_entity_tokens_as_targets:
-            if dataset_config.entity_special_tokens:
-                self.gene_special_token_id = tokenizer.vocab['<|gene token|>']
-                self.disease_special_token_id = tokenizer.vocab['<|disease token|>']
-                self.relation_special_token_id = tokenizer.vocab['<|relation token|>']
-                self.no_entity_special_token_id = tokenizer.vocab['<|no entity token|>']
-            else:
-                self.gene_special_token_id = tokenizer.vocab['Ġgene']
-                self.disease_special_token_id = tokenizer.vocab['Ġdisease']
-                self.relation_special_token_id = tokenizer.vocab['Ġrelation']
-                self.no_entity_special_token_id = tokenizer.vocab['Ġnull']
         self.bidirectional_attention_in_entity_tokens = dataset_config.bidirectional_attention_in_entity_tokens
         self.shift_entity_tokens = dataset_config.shift_entity_tokens
         # save the data to a {split_name}_gold.json file
@@ -392,138 +376,3 @@ class BioTriplexNERRELDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, index):
-        IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
-        # self.input_seen.add(index)
-
-        item = self.data[index]
-        debug = False #"4636297_Discussion_sentence_0" #"3979708_Introduction_sentence_0"  # (item["doc_key"] == "9818593_3. Results_sentence_0")
-        # prompt = item['input']#f"item['input']\n\n"
-        prompt_prefix, prompt_input, prompt_suffix = self.input_to_prompt(item["input"])
-        prompt = prompt_prefix + prompt_input + prompt_suffix
-        # example = prompt + item["output"]
-        example = prompt + "\n### Response:\n" + item["output"]
-        if self.upweight_minority_class:
-            if item["output"] != "[]":
-                weight = POSITIVE_WEIGHT
-            else:
-                weight = NEGATIVE_WEIGHT
-        else:
-            weight = 1.0
-        example = self.tokenizer.encode(example)
-        example.append(self.tokenizer.eos_token_id)
-        example = torch.tensor(example, dtype=torch.int64)
-        # self.longest_input = max(self.longest_input, example.shape[0])
-        if self.max_words is not None:
-            raise NotImplementedError("max_words is not implemented")
-            padding = self.max_words - example.shape[0]
-            if padding > 0:
-                example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
-            elif padding < 0:
-                example = example[: self.max_words]
-                # self.num_truncated_examples += 1
-        labels = copy.deepcopy(example)
-        if self.use_entity_tokens_as_targets:
-            prompt_prefix = self.tokenizer.encode(prompt_prefix)
-            prompt_input = self.tokenizer(prompt_input, add_special_tokens=False, return_offsets_mapping=True)
-            prompt_offsets_mapping = prompt_input["offset_mapping"]
-            prompt_input = prompt_input["input_ids"]
-            prompt_suffix = self.tokenizer.encode(prompt_suffix, add_special_tokens=False)
-            labels[:len(prompt_prefix)] = -1
-            labels[len(prompt_prefix): len(prompt_prefix) + len(prompt_input)] = self.no_entity_special_token_id
-            bidirectional_region_start = len(prompt_prefix)
-            bidirectional_region_end = len(prompt_prefix) + len(prompt_input)
-            if debug:
-                # print all entity spans:
-                for entity in item["entities"]:
-                    print(entity[0], entity[1], entity[2], f"entity_span: {item['input'][entity[0]: entity[1]]}end")
-            genes_indexes, diseases_indexes, relations_indexes = self.get_entity_indexes(item["entities"],
-                                                                                         prompt_offsets_mapping,
-                                                                                         index_offset=len(prompt_prefix),
-                                                                                         debug=debug)
-            assert len(prompt_prefix) + len(prompt_input) + len(prompt_suffix) == len(self.tokenizer.encode(prompt)) # TODO remove this assert
-            if self.shift_entity_tokens:
-                # shift the entity tokens to the right by one so that the model can predict the entity token
-                # at the start of the entity span
-                genes_indexes = [idx + 1 for idx in genes_indexes]
-                diseases_indexes = [idx + 1 for idx in diseases_indexes]
-                relations_indexes = [idx + 1 for idx in relations_indexes]
-            labels[genes_indexes] = self.gene_special_token_id
-            labels[diseases_indexes] = self.disease_special_token_id
-            labels[relations_indexes] = self.relation_special_token_id
-            labels[len(prompt_prefix) + len(prompt_input): len(prompt_prefix) + len(prompt_input) + len(prompt_suffix)] = -1
-        else:
-            prompt = torch.tensor(self.tokenizer.encode(prompt), dtype=torch.int64)
-            labels[:len(prompt)] = -1
-        example_mask = example.ge(0)
-        label_mask = labels.ge(0)
-        example[~example_mask] = 0
-        labels[~label_mask] = IGNORE_INDEX
-        example_mask = example_mask.float()
-        # label_mask = label_mask.float()
-        # example[example == -100] = self.tokenizer.pad_token_id
-        # labels[labels == -100] = self.tokenizer.pad_token_id
-
-        if debug:
-            # print each word with its label and token id if labels != -100
-            for i in range(len(self.tokenizer.encode(prompt))):
-                if labels[i].item() not in [-1, -100, 128259]:
-                    print(self.tokenizer.decode(example[i].item()), labels[i].item(), example[i].item())
-            print(item["doc_key"])
-            for entity in item["entities"]:
-                print(item["input"][entity[0]: entity[1]], entity[2], "entity_span: ", item["input"][entity[0]: entity[1]])
-
-        assert len(example) == len(labels)
-        # print("input_ids:", example.tolist())
-        # print("labels:", labels.tolist())
-        # print("attention_mask:", example_mask.tolist())
-        if self.bidirectional_attention_in_entity_tokens and self.use_entity_tokens_as_targets:
-            # allows for bidirectional attention in entity tokens by returning the start and end of the entity tokens
-            # in the input_ids tensor
-            return {
-                "input_ids": example.tolist(),
-                "labels": labels.tolist(),
-                "attention_mask": example_mask.tolist(),
-                "weight": weight,
-                "bidirectional_region_start": bidirectional_region_start,
-                "bidirectional_region_end": bidirectional_region_end
-                # "doc_key": item["doc_key"],
-                # "label_mask": label_mask
-            }
-        return {
-            "input_ids": example.tolist(),
-            "labels": labels.tolist(),
-            "attention_mask": example_mask.tolist(),
-            "weight": weight
-            # "doc_key": item["doc_key"],
-            # "label_mask": label_mask
-        }
-
-
-if __name__ == "__main__":
-    import transformers
-    from transformers import AutoTokenizer
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
-    tokenizer.add_special_tokens({"additional_special_tokens": ["<|gene token|>",
-                                                                "<|disease token|>",
-                                                                "<|relation token|>",
-                                                                "<|no entity token|>"]})
-    dataset_config = biotriplex_nerrel_dataset
-    dataset_config.use_entity_tokens_as_targets = False
-    for mode in "train", "val", "test":
-        dataset = BioTriplexNERRELDataset(dataset_config, tokenizer, mode, max_words=None,)
-        # print number of positive and negative examples (with weight 1 and 0.1 respectively)
-        num_positive = 0
-        num_negative = 0
-        for i in range(len(dataset)):
-            if dataset[i]["weight"] == POSITIVE_WEIGHT:
-                num_positive += 1
-            else:
-                num_negative += 1
-        print("MODE:", mode)
-        print(num_positive, num_negative)
-        # print len of longest input
-        max_len = 0
-        for i in range(len(dataset)):
-            max_len = max(max_len, len(dataset[i]["input_ids"]))
-        print(max_len)
